8e5433849eba963a5a4a8f84f420cffad91e9619
Tom <bballts14@gmx.de>
2016-05-30 20:06:06 +1000

    Integrate with a Slack Bot.

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 // ENV
-var buildkite_token = process.env.BUILDKITE_TOKEN;
+var buildkiteToken = process.env.BUILDKITE_TOKEN;
+var slackBotToken = process.env.SLACK_BOT_TOKEN;
 
+// Slack Setup
+var slackChannel = "#deploy-prod";
+var Botkit = require("botkit");
+var controller = Botkit.slackbot();
+var bot = controller.spawn({
+  token: slackBotToken
+});
+
+bot.startRTM(function(err, bot, payload) {
+  if (err) {
+    throw new Error("Could not connect to Slack: " + err);
+  }
+
+  console.log("Slack Bot running");
+});
+
+// Express Setup
 var https      = require("https");
 var express    = require("express");
 var bodyParser = require("body-parser");
@@ -8,11 +26,12 @@ var bodyParser = require("body-parser");
 var app = express();
 app.use(bodyParser.json());
 
+// App
 app.post("/build-finished", function(req, res) {
   console.log("Received POST on /build-finished", req.headers, req.body);
 
   // Verify token
-  if (req.headers["x-buildkite-token"] != buildkite_token) {
+  if (req.headers["x-buildkite-token"] != buildkiteToken) {
     console.log("Invalid buildkite token");
     return res.status(401).send("Invalid token");
   }
@@ -23,6 +42,12 @@ app.post("/build-finished", function(req, res) {
     var pipeline = req.body.pipeline;
 
     console.log("Build finished: " + pipeline.slug + " -> " + build.state);
+
+    if (build.state == "passed") {
+      bot.say({ text: "cancel deploy-prod", channel: slackChannel });
+    } else {
+      bot.say({ text: "steal deploy-prod", channel: slackChannel });
+    }
   }
 
   res.send("AOK");
@@ -31,3 +56,4 @@ app.post("/build-finished", function(req, res) {
 app.listen(process.env.PORT || 3000, function() {
   console.log("Express listening on port", this.address().port);
 });
+
